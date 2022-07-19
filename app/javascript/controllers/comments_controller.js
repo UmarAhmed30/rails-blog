e0fa2be60e40541d08f115e5836976b94881cea9
UmarAhmed30 <tumarahmed@gmail.com>
2022-07-19 11:30:55 +0530

    Edit button fixed

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -13,6 +13,7 @@ export default class extends Controller {
     event.stopPropagation();
     const formID = event.params["form"];
     const commentBodyID = event.params["body"];
+    const editButtonID = event.params["edit"];
 
     const form = document.getElementById(formID);
     form.classList.toggle("d-none");
@@ -20,5 +21,23 @@ export default class extends Controller {
 
     const commentBody = document.getElementById(commentBodyID);
     commentBody.classList.toggle("d-none");
+
+    const editButton = document.getElementById(editButtonID);
+    this.toggleEditButton(editButton);
+  }
+
+  toggleEditButton(editButton) {
+    if (editButton.innerText = "Edit") {
+      editButton.innerText = "Cancel";
+      this.toggleEditButtonClass(editButton);
+    } else {
+      editButton.innerText = "Edit";
+      this.toggleEditButtonClass(editButton);
+    }
+  }
+
+  toggleEditButtonClass(editButton) {
+    editButton.classList.toggle("btn-warning");
+    editButton.classList.toggle("btn-secondary");
   }
 }
